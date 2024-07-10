605a2aebc4f996cdc44f298decfdcfd9be80561f
Vinny Martins <martins.vds@outlook.com>
2024-07-10 19:23:39 +0000

    Refactor createVariables function to handle errors when creating variables

--- scripts/github/create-variables.js
+++ scripts/github/create-variables.js
@@ -7,63 +7,63 @@
  * 
 */
 export default async function createVariables({ github, context }, environment, variables) {
-    const owner = context.repo.owner
-    const repository = context.repo.repo
+    const owner = context.repo.owner;
+    const repository = context.repo.repo;
 
     if (!environment) {
-        throw new Error('environment is required')
+        throw new Error('environment is required');
     }
 
     if (!Array.isArray(variables)) {
-        throw new Error('variables must be an array')
+        throw new Error('variables must be an array');
     }
 
-    console.log(`Creating variables for environment ${environment}...`)
+    console.log(`Creating variables for environment ${environment}...`);
 
-    await Promise.allSettled(variables.map(variable => async () => {
-        try {
-            console.log(`Creating variable ${variable.name}...`)
-            await github.request(`POST /repos/${owner}/${repository}/environments/${environment}/variables`, {
-                owner: owner,
-                repo: repository,
-                environment_name: environment,
-                name: variable.name,
-                value: variable.value,
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28'
+    try {
+        const results = await Promise.allSettled(variables.map(variable => async () => {
+            try {
+                console.log(`Creating variable ${variable.name}...`)
+                await github.request(`POST /repos/${owner}/${repository}/environments/${environment}/variables`, {
+                    owner: owner,
+                    repo: repository,
+                    environment_name: environment,
+                    name: variable.name,
+                    value: variable.value,
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                })
+            } catch (error) {
+                if (error.status !== 409) {
+                    throw error
                 }
-            })
-        } catch (error) {
-            if (error.status !== 409) {
-                throw error
+
+                console.log(`Updating variable ${variable.name}...`)
+
+                await github.request(`PATCH /repos/${owner}/${repository}/environments/${environment}/variables/${variable.name}`, {
+                    owner: owner,
+                    repo: repository,
+                    environment_name: environment,
+                    name: variable.name,
+                    value: variable.value,
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                })
             }
+        }));
 
-            console.log(`Updating variable ${variable.name}...`)
-
-            await github.request(`PATCH /repos/${owner}/${repository}/environments/${environment}/variables/${variable.name}`, {
-                owner: owner,
-                repo: repository,
-                environment_name: environment,
-                name: variable.name,
-                value: variable.value,
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28'
-                }
-            })
-        }
-    }))
-        .then(results => {
-            results.forEach(result => {
-                if (result.status === 'fulfilled') {
-                    console.log('Variable created successfully', result.value)
-                } else {
-                    console.log('Variable creation failed', result.reason)
-                }
-            })
-        })
-        .catch(error => {
-            console.log('Error creating variables', error)
-        }).finally(() => {
-            console.log('Finished creating variables')
-        })
+        results.forEach(result => {
+            if (result.status === 'fulfilled') {
+                console.log('Variable created successfully', result.value)
+            } else {
+                console.log('Variable creation failed', result.reason)
+            }
+        });
+    } catch (error) {
+        console.log('Error creating variables', error);
+    } finally {
+        console.log('Finished creating variables');
+    }
 }
