65c9ad8cbceb9b84fae9062eeaf6ff2c2f5e2a19
Vinny Martins <youmartins.vds@outlook.com>
2024-07-10 17:13:03 +0000

    refactor

---
+++ scripts/github/temp.js
@@ -0,0 +1,69 @@
+const variables = [
+    {
+        name: 'GALLERY_NAME',
+        value: '${{ steps.deploy.outputs.galleryName }}'
+    },
+    {
+        name: 'DEV_CENTER_NAME',
+        value: '${{ steps.deploy.outputs.devCenterName }}'
+    },
+    {
+        name: 'IMAGE_BUILDER_MANAGED_IDENTITY_NAME',
+        value: '${{ steps.deploy.outputs.imageBuilderId }}'
+    },
+    {
+        name: 'BASE_DEV_BOX_DEFINITION_NAME',
+        value: '${{ steps.deploy.outputs.devBoxDefinitionName }}'
+    },
+    {
+        name: 'DEV_CENTER_ATTACHED_NETWORK',
+        value: '${{ steps.deploy.outputs.devCenterAttachedNetworkName }}'
+    }
+]
+
+export default ({ github, context, environment, variables }) => {
+    const owner = context.repo.owner
+    const repository = context.repo.repo
+
+    if (!environment) {
+        throw new Error('environment is required')
+    }
+
+    if (!Array.isArray(variables)) {
+        throw new Error('variables must be an array')
+    }
+
+    Promise.allSettled(variables.map(variable => async () => {
+        try {
+            console.log(`Creating variable ${variable.name}...`)
+            await github.request(`POST /repos/${owner}/${repository}/environments/${environment}/variables`, {
+                owner: owner,
+                repo: repository,
+                environment_name: environment,
+                name: variable.name,
+                value: variable.value,
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            })
+        } catch (error) {
+            if (error.status === 409) {
+                console.log(`Updating variable ${variable.name}...`)
+                await github.request(`PATCH /repos/${owner}/${repository}/environments/${environment}/variables/${variable.name}`, {
+                    owner: owner,
+                    repo: repository,
+                    environment_name: environment,
+                    name: variable.name,
+                    value: variable.value,
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                })
+            }
+        }
+    })).catch(error => {
+        console.error('Error creating variables', error)
+    }).finally(() => {
+        console.log('Finished creating variables')
+    })
+}
